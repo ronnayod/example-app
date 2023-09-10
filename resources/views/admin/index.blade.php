7bc387f675bc1056ad7d22dcd1c2d0e18859b2d8
ronnayod <gaen172543@gmail.com>
2023-09-10 16:10:11 +0700

    update2

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,33 @@
               <title>admin</title>
 </head>
 <body>
-              <h1>ยินดีต้อนรับแอดมิน</h1>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem iure, mollitia quis a nemo laborum, esse sit architecto animi iste rerum vero consequatur tenetur aspernatur fugit quisquam obcaecati, facere nisi?</p>
+              <?php
+
+              $user = "Ronnayod";
+              $arr = array("Home","Member","About");
+
+              ?>
+              
+              @if($user == "Ronnayod")
+                            <h1>ยินดีต้อนรับแอดมิน</h1>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem iure, mollitia quis a nemo laborum, esse sit architecto animi iste rerum vero consequatur tenetur aspernatur fugit quisquam obcaecati, facere nisi?</p>
+                            <h2>{{$user}}</h2>
+
+              @else
+              <h2>ผู้ใช้ท่านนี้ไม่เป็น Admin</h2>              
+              @endif
+
+
+              @foreach ($arr as $menu)
+                            <a href="">{{$menu}}</a>                  
+              @endforeach
+
+              
+              <ul>
+              @for($i=1;$i<=5;$i++)
+                            <li>{{$i}}</li>
+              @endfor
+              </ul>
+            
 </body>
 </html>
