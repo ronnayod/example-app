2ba119a216ad2a8dc6d5d8b5c45a9ad57c25281e
ronnayod <gaen172543@gmail.com>
2023-09-10 15:13:52 +0700

    up3

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//การสร้าง Route
+Route::get('/about', function () {
+    echo "ยินเีต้อนรับ";
+});
+
+Route::get('/users' , function(){
+    echo "Hello Ronnayod";
+});
